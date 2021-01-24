105548d7334abccbb836a758bf717813f7308c90
M. Can Tuncer <mcantuncerr@gmail.com>
2021-01-24 16:16:29 +0300

    me query is added.

--- src/api/user/user.resolver.ts
+++ src/api/user/user.resolver.ts
@@ -6,12 +6,16 @@ import { UserManager } from '../../models/user/manager';
 import { verify } from 'argon2';
 import { ApolloError } from 'apollo-server-express';
 import { CheckResponse, LoginInput } from './models';
-import { UserHelpers } from './helpers';
-import common from '../../../config/common';
-import { COOKIE_OPTIONS, SESSION_COOKIE_NAME } from '../../utils/constants';
 
 @Resolver(() => User)
 export default class {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req }): Promise<User | null> {
+    if (!req.session.userId) return null;
+
+    return await UserModel.findOne({ _id: req.session.userId }).exec();
+  }
+
   @Query(() => CheckResponse)
   async checkEmail(@Arg('email', () => String!) email: string): Promise<CheckResponse> {
     const user = await UserModel.findOne({ email }).lean();
@@ -22,13 +26,13 @@ export default class {
   }
 
   @Mutation(() => User)
-  async register(@Ctx() { res }, @Arg('input') input: UserInput): Promise<User | null> {
+  async register(@Ctx() { req }, @Arg('input') input: UserInput): Promise<User | null> {
     copyFields(input, 'id', '_id');
     const user = await UserManager.registerUser(input);
 
-    const token = UserHelpers.getToken(user, common.deploy_host);
-    res.cookie(SESSION_COOKIE_NAME, token, { httpOnly: true, sameSite: true });
-
+    // const token = UserHelpers.getToken(user, common.deploy_host);
+    // res.cookie(SESSION_COOKIE_NAME, token, { httpOnly: true, sameSite: true });
+    req.session.userId = user.id;
     return user;
   }
 
@@ -38,10 +42,9 @@ export default class {
     const user = await UserModel.findOne({ email: email }).exec();
 
     if (user && (await verify(user.password, input.password))) {
-      const token = UserHelpers.getToken(user, common.deploy_host);
-      res.cookie(SESSION_COOKIE_NAME, token, COOKIE_OPTIONS);
+      // const token = UserHelpers.getToken(user, common.deploy_host);
+      // res.cookie(SESSION_COOKIE_NAME, token, COOKIE_OPTIONS);
       req.session.userId = user.id;
-
       return user;
     } else {
       throw new ApolloError('Username or password is incorrect', 'USERNAME_OR_PASSWORD_IS_INCORRECT');
